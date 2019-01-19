291b3f54304a348d482f14caca8f838d2bcd33fb
Samidh Patel <spatel13456@gmail.com>
2019-01-19 18:01:18 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,6 +10,16 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+my_songs = {
+ "Go Go GO" => '< /home/wise-channel-2810/jukebox-cli-atlanta-web-career-012819 >/jukebox-cli/audio/Emerald-Park/01.mp3',
+ "LiberTeens" => '< /home/wise-channel-2810/jukebox-cli-atlanta-web-career-012819  >/jukebox-cli/audio/Emerald-Park/02.mp3',
+ "Hamburg" =>  '< /home/wise-channel-2810/jukebox-cli-atlanta-web-career-012819 >/jukebox-cli/audio/Emerald-Park/03.mp3',
+ "Guiding Light" => '< /home/wise-channel-2810/jukebox-cli-atlanta-web-career-012819 >/jukebox-cli/audio/Emerald-Park/04.mp3',
+ "Wolf" => '< /home/wise-channel-2810/jukebox-cli-atlanta-web-career-012819 >/jukebox-cli/audio/Emerald-Park/05.mp3',
+ "Blue" => '< /home/wise-channel-2810/jukebox-cli-atlanta-web-career-012819 >/jukebox-cli/audio/Emerald-Park/06.mp3',
+ "Graduation Failed" => '< /home/wise-channel-2810/jukebox-cli-atlanta-web-career-012819 >/jukebox-cli/audio/Emerald-Park/07.mp3'
+ }
+
 # def say_hello(name)
   # "Hi #{name}!"
 # end
